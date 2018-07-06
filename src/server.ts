d802f701e523b8c19fd43891bff68b896509925b
mr5 <mr5.simple@gmail.com>
2018-07-06 21:59:12 +0800

    add TZ env

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
 import EventDispatcher from './EventDispatcher';
+import moment from 'moment-timezone';
 
 require('source-map-support/register');
-
+if (process.env.TZ) {
+  moment.tz.setDefault(process.env.TZ);
+}
 const config = yaml.load(
   fs.readFileSync(path.join(__dirname, '/../config/.kubewatcher.yml')).toString('utf-8')
 );
